204b3e2acf027cab83960fccf8ea088d23f6ea48
caleb <iriiises@gmail.com>
2022-12-09 20:16:19 -0800

    finished and added comments

--- main.py
+++ main.py
@@ -13,34 +13,36 @@ import math
 
 smalldata = np.loadtxt("./CS170_Small_Data__93.txt")
 largedata = np.loadtxt("./CS170_Large_Data__15.txt")
-distance_cache = {}
 
-
+#nn distance function using euclidean distance
 def dist(obj, data, fts):
     distance = 0
-    match = (obj, data, frozenset(fts))
-    for key, value in distance_cache.items():
-        if key == match:
-            return distance_cache[key]
     for i in fts:
         distance += (obj[i-1] - data[i-1])**2
     return np.sqrt(distance)
         
-        
+#feature search function
 def feature_search_demo(data):
-    
+            
+    #initializes the list of current features and best set of features
     current = []
     best_set = []
     best_best_acc = 0
+    
     print("\nBeginning search...\n")
+    
+    #loops over features to consider
     for i in range(1, data.shape[1]):
         print(f"On level {i} of the search tree")
         addedft = []
         bestacc = 0
+        
+        #loops over possible features to add
         for j in range(1, data.shape[1]):
             if(j not in current):
                 print(f"  Consider adding feature {j}")
                 
+                #computing accuracy of the feature set with the added feature and compare to the current best accuracy
                 acc = cross_validation(data, current, j)
                 if acc > bestacc:
                    bestacc = acc
@@ -49,17 +51,22 @@ def feature_search_demo(data):
         current.append(addedft)          
         print(f"\nOn level {i} I added the feature {addedft} to the feature set which is now {current}")    
         print(f"The feature had an accuracy of {round(bestacc*100, 3)}%\n")
+        
+        #finds the best accuracy out of all feature sets computed
         if bestacc > best_best_acc:
             best_best_acc = bestacc
             best_set.append(addedft)
         
     print(f"\nBest feature set is {best_set} with an accuracy of {round(best_best_acc*100, 3)}%")
-                       
+   
+#backwards feature search function, very similar to the forward search function             
 def backwards_search_demo(data):
     
     current = []
     best_set = []
     best_best_acc = 0
+    
+    #adds all features to the current set
     for i in range(1, data.shape[1]):
         current.append(i)
         best_set.append(i)
@@ -78,6 +85,7 @@ def backwards_search_demo(data):
                    bestacc = acc
                    addedft = j
         
+        #removes ft from current set
         current.remove(addedft)          
         print(f"\nOn level {i} I removed the feature {addedft} to the feature set which is now {current}")    
         print(f"The feature had an accuracy of {round(bestacc*100, 3)}%\n")
@@ -88,18 +96,28 @@ def backwards_search_demo(data):
     print(f"\nBest feature set is {best_set} with an accuracy of {round(best_best_acc*100, 3)}%")
                        
 
-
+#computes accuracy of the classifier
 def cross_validation(data, current, addedft):
     classified = 0
+    
+    #copies current feature set and the added feature into the curr array
     curr = 0
     curr = current[:]
     curr.append(addedft)
+    
+    #loop over all instances in the dataset
     for i in range(0, data.shape[0]):
+        
+        #object to classify, is row i with all features from row one on
         obj_to_classify = data[i, 1:]
+        
+        #label i
         label_obj_to_classify = data[i, 0]
         
         nn_dist = 999999
         nn_loc = 999999
+        
+        #loops through all instances and calculates nn distances
         for j in range(data.shape[0]):
             
             if j != i:
@@ -109,12 +127,14 @@ def cross_validation(data, current, addedft):
                     nn_dist = distance
                     nn_loc = j
                     nn_label = data[nn_loc, 0]
-                    
+        
+        #calculates total classified labels            
         if label_obj_to_classify == nn_label:
             classified+=1
     print(f"  Accuracy of set {curr}: {round(classified / data.shape[0], 2)}")        
     return classified / len(data)
-                
+
+#essentially the same as forward except it will remove instead of appending features                
 def bw_cross_validation(data, current, addedft):
     classified = 0
     curr = 0
